f7f124b9d5ef736500f0fcd615cf9a3201d2e131
YuraSuper2048 <kremn2018@yandex.ru>
2023-11-22 20:42:38 +0800

    added the ability to select between floats and base64 encoding formats into embedding api

--- SimpleOpenAi/Endpoints/Embeddings.cs
+++ SimpleOpenAi/Endpoints/Embeddings.cs
@@ -15,8 +15,10 @@ public class Embeddings
     /// <summary>
     /// Creates an embedding vector representing the input text.
     /// https://platform.openai.com/docs/api-reference/embeddings/create
+    /// <br/><br/>
+    /// Prefer to use <see cref="CreateAsync(string,string,string?,System.Threading.CancellationToken)"/> for most cases.
     /// </summary>
-    public async Task<Result> CreateAsync(
+    public async Task<FloatsResult> CreateFloatsAsync(
         string input,
         string model = "text-embedding-ada-002",
         string? user = null,
@@ -30,17 +32,19 @@ public class Embeddings
             { "encoding_format", "float" }
         };
 
-        var result = await _openAiApiRequestHandler.SendRequestAsync<Result>(
+        var result = await _openAiApiRequestHandler.SendRequestAsync<FloatsResult>(
             HttpMethod.Post, "/embeddings", parameters, cancellationToken);
 
         return result;
     }
 
     /// <summary>
-    /// Creates an embedding vector representing the input texts.
+    /// Creates an embedding vector representing the input texts, using the output encoding format "float"
     /// https://platform.openai.com/docs/api-reference/embeddings/create
+    /// <br/><br/>
+    /// Prefer to use <see cref="CreateAsync(IEnumerable&lt;string&gt;,string,string?,System.Threading.CancellationToken)"/> for most cases.
     /// </summary>
-    public async Task<Result> CreateAsync(
+    public async Task<FloatsResult> CreateFloatsAsync(
         IEnumerable<string> input,
         string model = "text-embedding-ada-002",
         string? user = null,
@@ -54,7 +58,55 @@ public class Embeddings
             { "encoding_format", "float" }
         };
 
-        var result = await _openAiApiRequestHandler.SendRequestAsync<Result>(
+        var result = await _openAiApiRequestHandler.SendRequestAsync<FloatsResult>(
+            HttpMethod.Post, "/embeddings", parameters, cancellationToken);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Creates an embedding vector representing the input texts.
+    /// https://platform.openai.com/docs/api-reference/embeddings/create
+    /// </summary>
+    public async Task<Base64Result> CreateAsync(
+        string input,
+        string model = "text-embedding-ada-002",
+        string? user = null,
+        CancellationToken cancellationToken = default)
+    {
+        var parameters = new Dictionary<string, object?>
+        {
+            { "input", input },
+            { "model", model },
+            { "user", user },
+            { "encoding_format", "base64" }
+        };
+
+        var result = await _openAiApiRequestHandler.SendRequestAsync<Base64Result>(
+            HttpMethod.Post, "/embeddings", parameters, cancellationToken);
+
+        return result;
+    }
+
+    /// <summary>
+    /// Creates an embedding vector representing the input texts.
+    /// https://platform.openai.com/docs/api-reference/embeddings/create
+    /// </summary>
+    public async Task<Base64Result> CreateAsync(
+        IEnumerable<string> input,
+        string model = "text-embedding-ada-002",
+        string? user = null,
+        CancellationToken cancellationToken = default)
+    {
+        var parameters = new Dictionary<string, object?>
+        {
+            { "input", input },
+            { "model", model },
+            { "user", user },
+            { "encoding_format", "base64" }
+        };
+
+        var result = await _openAiApiRequestHandler.SendRequestAsync<Base64Result>(
             HttpMethod.Post, "/embeddings", parameters, cancellationToken);
 
         return result;
@@ -63,15 +115,59 @@ public class Embeddings
     /// <summary>
     /// https://platform.openai.com/docs/api-reference/embeddings/object
     /// </summary>
-    public record struct EmbeddingObject
+    public record struct FloatsEmbeddingObject
     (
         [property: JsonProperty("object")] string Object,
         [property: JsonProperty("index")] int Index,
         [property: JsonProperty("embedding")] float[] Embedding
     );
-    public record struct Result
+    public record struct FloatsResult
     (
-        [property: JsonProperty("data")] IReadOnlyList<EmbeddingObject> Data,
+        [property: JsonProperty("data")] IReadOnlyList<FloatsEmbeddingObject> Data,
+        [property: JsonProperty("model")] string Model,
+        [property: JsonProperty("object")] string Object,
+        [property: JsonProperty("usage")] Usage Usage
+    )
+    {
+        /// <summary>
+        ///     Returns the first embedding
+        /// </summary>
+        public float[] Embedding => Data[0].Embedding;
+    }
+
+    /// <summary>
+    /// https://platform.openai.com/docs/api-reference/embeddings/object
+    /// </summary>
+    public record struct Base64EmbeddingObject
+    (
+        [property: JsonProperty("object")] string Object,
+        [property: JsonProperty("index")] int Index,
+        [property: JsonProperty("embedding")] string EmbeddingBase64
+    )
+    {
+        /// <summary>
+        ///     Returns the first embedding
+        /// </summary>
+        public float[] Embedding
+        {
+            get
+            {
+                var bytes = Convert.FromBase64String(EmbeddingBase64);
+                
+                var floats = new float[bytes.Length / 4];
+                for (var i = 0; i < floats.Length; i++)
+                {
+                    floats[i] = BitConverter.ToSingle(bytes, i * 4);
+                }
+
+                return floats;
+            }
+        }
+    }
+    
+    public record struct Base64Result
+    (
+        [property: JsonProperty("data")] IReadOnlyList<Base64EmbeddingObject> Data,
         [property: JsonProperty("model")] string Model,
         [property: JsonProperty("object")] string Object,
         [property: JsonProperty("usage")] Usage Usage
